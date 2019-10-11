ba869546f58c7b3a2b7d5dbe4733b37c949016e5
nimishagarwal76 <nimishagarwal76@gmail.com>
2019-10-11 18:14:37 +0530

    More Contracts

--- contracts/UserRegistration.sol
+++ contracts/UserRegistration.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.5.0;
 contract UserRegistration {
     address[] public users;
 
-    // mapping (address => string) addressWillMapping;
+    mapping (address => string) addressWillMapping;
 
     function setUser(address _user) public{
         uint id = users.push(_user);
@@ -14,6 +14,24 @@ contract UserRegistration {
 		  return users;
     }
 
+    function isRegistered(address _user) public view returns(bool) {
+      for(uint i = 0; i < users.length; ++i)
+      {
+        if(users[i] == _user)
+        {
+          return true;
+        }
+      }
+      return false;
+    }
+
+    function addWill(string memory _will) public{
+      addressWillMapping[msg.sender] = _will;
+    }
+
+    function getWill(address _user) public view returns(string memory) {
+      return addressWillMapping[_user];
+    }
 
 }
 
