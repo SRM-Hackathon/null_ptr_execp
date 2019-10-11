a711a188fd858a11b1f8e524336539ca5752f9a2
shakeabi <abishake.110899@gmail.com>
2019-10-11 16:22:20 +0530

    Fix: Edit app header and add alert for metamask

--- src/js/app.js
+++ src/js/app.js
@@ -45,7 +45,8 @@ App = {
     }
     // If no injected web3 instance is detected, fall back to Ganache
     else {
-      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+      // App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+      alert("Please install web3 wallet like metamask");
     }
     web3 = new Web3(App.web3Provider);
 
